9667e819c95da7054034685ee25afd9419a09d34
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-27 21:06:53 +0900

    Update: 흐름 파악

--- 3.3/app.js
+++ 3.3/app.js
@@ -1,17 +1,25 @@
-// redux만들기
-
 function createStore() {
   // store를 모아서 넘겨줌
   let state;
 
-  workerData(state);
+  // 2. worker호출
+  function send() {
+    state = worker(state);
+  }
 
-  return {};
+  return {
+    send,
+  };
 }
 
+// 3. 상태 변경
 function worker(state) {
-  // do sometiong
+  // 새로운 상태 반환 => 항상 복사해서 Deep copy해서 새로운 객체로 반환
+  return { ...state };
 }
 
 // Store는 상태를 담고 있다. 상태들을 묶고 있는 객체
-const store = createStore(state);
+const store = createStore(worker);
+
+// 1.호출
+store.send();
