f98e5e55d3de063ef5571d3cf9adb6cc46674e81
neo@zillinks.com <keunhey0107@gmail.com>
2023-10-27 21:20:25 +0900

    Update: 구독 발행 모델

--- 3.3/app.js
+++ 3.3/app.js
@@ -1,21 +1,28 @@
 function createStore() {
   // store를 모아서 넘겨줌
   let state;
+  // 구독할 테니 데이터가 바뀌면 그 함수를 호출해줘 (Pub and Subcribe)
+  let handlers;
 
   // 2. worker호출
   function send() {
     state = worker(state);
   }
 
+  function getState() {
+    return state;
+  }
+
   return {
     send,
+    getState,
   };
 }
 
 // 3. 상태 변경
-function worker(state) {
+function worker(state = { count: 0 }) {
   // 새로운 상태 반환 => 항상 복사해서 Deep copy해서 새로운 객체로 반환
-  return { ...state };
+  return { ...state, count: state.count + 1 };
 }
 
 // Store는 상태를 담고 있다. 상태들을 묶고 있는 객체
@@ -23,3 +30,5 @@ const store = createStore(worker);
 
 // 1.호출
 store.send();
+
+console.log(store.getState());
