c03f39f6d861b27c42cc4ef9ee5530fd54b63efc
neo@zillinks.com <keunhey0107@gmail.com>
2023-11-02 20:29:36 +0900

    Update: action type추가

--- 3.4/action-type.js
+++ 3.4/action-type.js
@@ -1,1 +1,3 @@
 export const INCREASE = 'increase';
+export const DECREASE = 'decrease';
+export const REASET = 'reset';
--- 3.4/actions.js
+++ 3.4/actions.js
@@ -1,4 +1,6 @@
-import { INCREASE } from './action-type';
+import { DECREASE, INCREASE, REASET } from './action-type';
 import { actionCreator } from './redux';
 
 export const increase = () => actionCreator(INCREASE);
+export const decrease = () => actionCreator(DECREASE);
+export const resett = () => actionCreator(REASET);
