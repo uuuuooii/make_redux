48ba4f17f30354cf8b90124d79e96d17d1594c24
neo@zillinks.com <keunhey0107@gmail.com>
2023-11-05 16:36:27 +0900

    Update: action 추가

--- 3.4/reducer.js
+++ 3.4/reducer.js
@@ -1,7 +1,13 @@
+import * as ActionType from './action-type';
+
 export function reducer(state = { count: 0 }, action) {
   switch (action.type) {
-    case 'increase':
+    case ActionType.INCREASE:
       return { ...state, count: state.count + 1 };
+    case ActionType.DECREASE:
+      return { ...state, count: state.count - 1 };
+    case ActionType.REASET:
+      return { ...state, count: 0 };
     default:
       return { ...state };
   }
