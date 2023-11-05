5bf48fa3035c9511a73d6613b2066bc249484e80
neo@zillinks.com <keunhey0107@gmail.com>
2023-11-05 16:43:07 +0900

    Update: initialize 추가

--- 3.4/reducer.js
+++ 3.4/reducer.js
@@ -1,6 +1,9 @@
 import * as ActionType from './action-type';
 
-export function reducer(state = { count: 0 }, action) {
+const InitializeState = { count: 0 };
+
+// 상태와 관련된 변경사항은 reducer함수만 체크 하면 됨
+export function reducer(state = InitializeState, action) {
   switch (action.type) {
     case ActionType.INCREASE:
       return { ...state, count: state.count + 1 };
